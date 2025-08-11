c50bb7518efec8969436af8d47e1fa067b100dd0
VN Phạm <nam.pv1805@gmail.com>
2025-08-11 23:00:52 +0200

    Fix bugs

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -21,8 +21,8 @@ describe('AppController', () => {
         it('Should return "Good Bye!"', () => {
             expect(appController.getGoodBye()).toBe('Good Bye!');
         });
-        // it('PORT should be 8000', () => {
-        //     expect(process.env.PORT).toBe('8000');
-        // });
+        it('PORT should be 8000', () => {
+            expect(process.env.PORT).toBe('8000');
+        });
     });
 });
